fd38e1486646fd04cfc5caca1b846d77e589d0f2
frank <frank.sprich@gmail.com>
2022-09-08 10:43:27 +0200

    Mockito with explicit mock creation and close

---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * {DESCRIPTION}
+ *
+ * @author Frank Sprich
+ */
+class StudentServiceTest {
+
+    @Mock private StudentRepository studentRepository;
+    private AutoCloseable autoCloseable;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void getAllStudents() {
+        // when
+        underTest.getAllStudents();
+        // then
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    @Disabled
+    void addStudent() {
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
