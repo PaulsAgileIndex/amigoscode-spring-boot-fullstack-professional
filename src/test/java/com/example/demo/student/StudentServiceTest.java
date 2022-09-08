c492bb351ab19f02308600192812dd5fba41677a
frank <frank.sprich@gmail.com>
2022-09-08 10:48:53 +0200

    Mockito with @ExtendWith(MockitoExtension.class) to handle mock creation and teardown

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,11 +1,11 @@
 package com.example.demo.student;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
 
@@ -14,22 +14,17 @@ import static org.mockito.Mockito.verify;
  *
  * @author Frank Sprich
  */
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock private StudentRepository studentRepository;
-    private AutoCloseable autoCloseable;
     private StudentService underTest;
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
     }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
-    }
 
     @Test
     void getAllStudents() {
