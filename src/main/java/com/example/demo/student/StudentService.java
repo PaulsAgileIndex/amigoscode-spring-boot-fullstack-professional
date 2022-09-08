aa9949905709181e4b9037fe0314b18bceec0ad5
frank <frank.sprich@gmail.com>
2022-09-08 11:01:08 +0200

    Mockito argument capture to verify that the inserted is equal to parameter

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -20,8 +20,7 @@ public class StudentService {
     }
 
     public void addStudent(Student student) {
-        Boolean existsEmail = studentRepository
-                .selectExistsEmail(student.getEmail());
+        Boolean existsEmail = studentRepository.selectExistsEmail(student.getEmail());
         if (existsEmail) {
             throw new BadRequestException(
                     "Email " + student.getEmail() + " taken");
--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -27,7 +29,7 @@ class StudentServiceTest {
 
 
     @Test
-    void getAllStudents() {
+    void testGetAllStudents() {
         // when
         underTest.getAllStudents();
         // then
@@ -35,12 +37,25 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void testAddStudent() {
+        // given
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+        // when
+        underTest.addStudent(student);
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
     @Test
     @Disabled
-    void deleteStudent() {
+    void testDeleteStudent() {
     }
 }
